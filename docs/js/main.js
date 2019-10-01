4863b6ddfc563d160a2e7194fee7d90148727874
Omid Reza Heidari <snip77.orh@gmail.com>
2019-10-01 17:31:59 +0330

    Delete unused files Delete year part of pages

--- docs/js/main.js
+++ docs/js/main.js
@@ -179,3 +179,9 @@
     fixedContentPos: false
   });
 })(jQuery);
+
+var viewMode = getCookie("view-mode");
+if(viewMode == "desktop")
+	viewport.setAttribute('content', 'width=1024');
+else if (viewMode == "mobile")
+	viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
